859fbb7e7e10c361e50c732e6202320720981535
Andrey Saulin <32382950+andsoft80@users.noreply.github.com>
2020-11-24 17:59:28 +0300

    indicator

--- db_service/assets/account.js
+++ db_service/assets/account.js
@@ -1,11 +1,15 @@
 function Account() {
     const [userData, setUserData] = useState({ name: '' });
-    const [companyData, setCompanyData] = React.useState({name:''});
+    const [companyData, setCompanyData] = React.useState({ name: '' });
     React.useEffect(() => {
         getUserData();
-    }, []);
-    function getUserData() {
         
+    }, []);
+    React.useEffect(() => {
+
+      }, [Tab]);
+    function getUserData() {
+
         axios.post('/userdbinfo', {
 
 
@@ -13,17 +17,17 @@ function Account() {
 
         }, { headers: { "Authorization": 'Bearer ' + getToken() } })
             .then(function (response) {
-                
+
                 setUserData(response.data);
-                
+
                 getCompanyById(response.data.companyid);
-                
+
 
 
             })
             .catch(function (error) {
                 // handle error
-                
+                alert(error);
                 if (error.message.indexOf('400') > 0) {
                     window.location = '/login.html';
                 }
@@ -34,16 +38,16 @@ function Account() {
                 // alert(typeof error.message);
             })
     }
-    const getCompanyById =(id)=>{
+    const getCompanyById = (id) => {
         axios.post('/table/companies/action/get', {
             id: id,
-            
+
 
         }, { headers: { "Authorization": 'Bearer ' + getToken() } })
             .then(function (response) {
-                
-                if(response.data[0])
-                setCompanyData(response.data[0]);
+
+                if (response.data[0])
+                    setCompanyData(response.data[0]);
 
 
             })
@@ -59,12 +63,85 @@ function Account() {
                 // alert(typeof error.message);
             })
     }
+
+
+    ///////////////////////////////////tabs
+
+    function TabPanel(props) {
+        const { children, value, index, ...other } = props;
+
+        return (
+            <div
+                role="tabpanel"
+                hidden={value !== index}
+                id={`simple-tabpanel-${index}`}
+                aria-labelledby={`simple-tab-${index}`}
+                {...other}
+            >
+                {value === index && (
+                    <Box p={3}>
+                        <Typography>{children}</Typography>
+                    </Box>
+                )}
+            </div>
+        );
+    }
+
+    // TabPanel.propTypes = {
+    //     children: PropTypes.node,
+    //     index: PropTypes.any.isRequired,
+    //     value: PropTypes.any.isRequired,
+    // };
+
+    function a11yProps(index) {
+        return {
+            id: `simple-tab-${index}`,
+            'aria-controls': `simple-tabpanel-${index}`,
+        };
+    }
+
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            flexGrow: 1,
+            backgroundColor: theme.palette.background.paper,
+        },
+    }));
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    ///////////////////////////////////////
     return (
+
 
         <div>
             <font size="5">Состояние счета</font>
             <br />
             {companyData.name}
+            <br /><br />
+            <div className={classes.root}>
+                <Paper position="static" square>
+                    
+                        <Tabs value={value} onChange={handleChange} aria-label="simple tabs example" indicatorColor="primary"
+                            textColor="primary" >
+                            <Tab label="Item One" {...a11yProps(0)} />
+                            <Tab label="Item Two" {...a11yProps(1)} />
+                            <Tab label="Item Three" {...a11yProps(2)} />
+                        </Tabs>
+                    
+                </Paper>
+                <TabPanel value={value} index={0}>
+                    Item One
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    Item Two
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                    Item Three
+                </TabPanel>
+            </div>
         </div>
 
 
@@ -79,4 +156,5 @@ ReactDOM.render(
         <Account />
     </ThemeProvider>,
     document.querySelector('#content_account'),
+
 );
